d99917b8331e5e5824d29e628561a48181bac6e8
Lucas Gomes de Araujo <lucasgomesdlno227@gmail.com>
2026-05-30 18:04:06 -0300

    limpando login

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,7 +10,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.css']
 })
 export class App implements OnInit, OnDestroy {
-  // Dados iniciais antes da placa enviar a primeira leitura
+  // --- DADOS DO DASHBOARD ---
+  // Começa com valores a zero até o Python enviar os dados reais
   sensorData: any = { 
     device_name: 'A carregar...', 
     gas_level_percentage: 0, 
@@ -20,15 +21,19 @@ export class App implements OnInit, OnDestroy {
 
   constructor(private http: HttpClient) {}
 
+  // ==========================================
+  // INÍCIO AUTOMÁTICO
+  // ==========================================
   ngOnInit() {
-    // Sem tela de login, começamos a ler o banco de dados imediatamente!
+    // Como tiramos o login, a leitura começa assim que o site abre!
     this.iniciarLeituraDoSensor();
   }
 
+  // --- LÓGICA DE LIGAÇÃO AO BACKEND (PYTHON) ---
   iniciarLeituraDoSensor() {
-    this.buscarDadosNoBanco(); // Lê a primeira vez
+    this.buscarDadosNoBanco(); // Lê imediatamente a primeira vez
     
-    // Continua a ler a cada 2 segundos
+    // Fica a ler o banco de dados a cada 2 segundos
     this.intervalId = setInterval(() => {
       this.buscarDadosNoBanco();
     }, 2000); 
@@ -37,21 +42,12 @@ export class App implements OnInit, OnDestroy {
   buscarDadosNoBanco() {
     this.http.get<any>('https://gasguard-backend.onrender.com/api/status').subscribe({
       next: (resposta) => {
-        // Se a API mandar uma lista, pegamos sempre a última leitura da fila
-        let dados = Array.isArray(resposta) ? resposta[resposta.length - 1] : resposta;
+        if (!resposta) return;
 
-        if (!dados) return; // Se vier vazio, ignora e tenta de novo depois
-
-        // Ligar as chaves exatas que o seu ESP32 está a enviar
-        this.sensorData.device_name = dados.id_dispositivo; 
-        this.sensorData.gas_level_percentage = dados.nivel_gas;
-
-        // Se o gás passar de 25, muda o painel para vermelho!
-        if (dados.nivel_gas >= 25) {
-          this.sensorData.status = 'Perigo';
-        } else {
-          this.sensorData.status = 'Seguro';
-        }
+        // Pegando exatamente as chaves que o Python está enviando
+        this.sensorData.device_name = resposta.device_name; 
+        this.sensorData.gas_level_percentage = resposta.gas_level_percentage;
+        this.sensorData.status = resposta.status;
       },
       error: (erro) => {
         console.error('Erro de conexão com o Render:', erro);
