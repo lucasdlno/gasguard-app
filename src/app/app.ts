d73b65e0c4d6724d0e50634e5573800659233622
Lucas Gomes de Araujo <lucasgomesdlno227@gmail.com>
2026-05-30 18:30:03 -0300

    Adicionando ChangeDetectorRef para forcar a atualizacao da tela

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core'; // <-- Importamos o despertador aqui!
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 
@@ -17,7 +17,10 @@ export class App implements OnInit, OnDestroy {
   };
   intervalId: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef // <-- Injetamos o despertador aqui!
+  ) {}
 
   ngOnInit() {
     this.iniciarLeituraDoSensor();
@@ -34,24 +37,24 @@ export class App implements OnInit, OnDestroy {
   buscarDadosNoBanco() {
     this.http.get<any>('https://gasguard-backend.onrender.com/api/status').subscribe({
       next: (resposta) => {
-        // 1. O ESPIÃO VOLTOU! Vamos ver tudo o que chega.
         console.log("DADOS RECEBIDOS DO RENDER:", resposta);
 
-        // 2. Garante que pega a última leitura se for uma lista
         let dados = Array.isArray(resposta) ? resposta[resposta.length - 1] : resposta;
 
-        // 3. Se por algum motivo vier vazio, o espião avisa!
         if (!dados) {
           console.log("Aviso: O banco não retornou dados válidos.");
           return; 
         }
 
-        // 4. ATUALIZAÇÃO DA TELA: Recriando o objeto inteiro para o Angular acordar
+        // Atualizamos os dados na memória...
         this.sensorData = {
           device_name: dados.device_name,
           gas_level_percentage: dados.gas_level_percentage,
           status: dados.status
         };
+
+        // ACORDA O ANGULAR À FORÇA! Isso obriga a tela HTML a piscar e mostrar os dados novos na mesma hora.
+        this.cdr.detectChanges(); 
       },
       error: (erro) => {
         console.error('Erro de conexão com o Render:', erro);
